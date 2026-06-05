6ca33f89642c01ed331fe961290851c57c460fe5
ayeeshakollere-pixel <ayeeshakollere@gmail.com>
2026-06-05 12:41:16 +0100

    Initialize main.jsx with React and KoreApp

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import KoreApp from "./components/KoreApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <KoreApp />
+  </React.StrictMode>
+);
